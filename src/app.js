c3526ad7b69b3fb792aa97fa635afd32633e5578
swiftcast <4298986+swiftcast@users.noreply.github.com>
2022-02-05 19:27:55 -0500

    add more forms and edge case for Farfetch’d apostrophe'

--- src/app.js
+++ src/app.js
@@ -11,115 +11,130 @@ client.login(process.env.TOKEN);
 var prefix = '.'
 
 client.on('messageCreate', message => {
-  if (message.content === '.ping') {
-    //msg.reply('Pong!');
-    message.channel.send('Pong!');
-  }
-
-  if (message.content === '.hi') {
-    //msg.reply('Pong!');
-    message.channel.send('hi ur qt');
-  }
-
-  if (message.content.startsWith(prefix + "count ")) {
-	//let beginIndex = 
-	let command = message.content.slice((prefix + "count ").length);
-
-	// Cleans input
-	command = command.toLowerCase()
-	let arr = command.split(' ')
-	let arrThumb = [...arr]; // Used for the embed thumbnail
-	arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-
-	// If it's a regional form noted by having a 2nd element in array
-	if (arr.length > 1) {
-		arr[1] = "(" + arr[1].charAt(0).toUpperCase() + arr[1].slice(1) + ")";
+	if (message.content === '.ping') {
+		//msg.reply('Pong!');
+		message.channel.send('Pong!');
 	}
 
-	command = arr.join(' ');
-
-	let pokemon = list.filter(poke => poke.name === command)[0];
-
-	if (pokemon == undefined) {
-		return;
+	if (message.content === '.hi') {
+		//msg.reply('Pong!');
+		message.channel.send('hi ur qt');
 	}
 
-	pokeEmbed.setTitle(`${pokemon.name}`);
+	if (message.content.startsWith(prefix + "count ")) {
+		//let beginIndex = 
+		let command = message.content.slice((prefix + "count ").length);
 
-	let types = ""
-	// Capitalize first letter of each type
-	for (let i = 0; i < pokemon.types.length; i++) {
-		if (i == 1) types += '/';
-		types += pokemon.types[i].charAt(0).toUpperCase() + pokemon.types[i].slice(1);
-	}
+		// Cleans input
+		command = command.toLowerCase()
+		let arr = command.split(' ')
+		let arrThumb = [...arr]; // Used for the embed thumbnail
 
-	pokeEmbed.setDescription(types);
-
-	debugger;
-	let fastMovesField = ""
-
-	// Creates a field for each charge move 
-	for (let i = 0; i < pokemon.counts.length; i++) {
-		for (let j = 0; j < pokemon.counts[i].fastMoves.length; j++) {
-			debugger;
-			fastMovesField += `**${pokemon.counts[i].fastMoves[j].name}**: ${pokemon.counts[i].fastMoves[j].counts}\n`; 
+		// Edge case bc of the apostrophe isn't the same as the one on the keyboard
+		if (arr[0].search('arfetch') > -1) {
+			arr[0] = 'Farfetch’d';
+			arrThumb[0] = 'farfetchd'
 		}
-		pokeEmbed.addField(pokemon.counts[i].chargedMove.name, fastMovesField, true);
-		fastMovesField = "";
-	}
 
-	if (arrThumb.length > 1) {
+		arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
 
-		switch(arrThumb[1]) {
-			case 'galarian':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1].slice(0, -3)}.gif`);
-				break;
-			case 'alolan':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1].slice(0, -1)}.gif`);
-				break;
-			case 'therian':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
-				break;
-			case 'armoured':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`);
-				break;
-			case 'sunny':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
-				break;
-			case 'rainy':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
-				break;
-			case 'snowy':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
-				break;
-			case 'normal':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`);
-				break;
-			/*ase 'crowned':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
-				break;
-			case 'hisuian':
-				pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1].slice(0, -2)}.gif`);
-				break;*/
-			default:
-				break;
+		// If it's a regional form noted by having a 2nd element in array
+		if (arr.length > 1) {
+			arr[1] = "(" + arr[1].charAt(0).toUpperCase() + arr[1].slice(1) + ")";
 		}
-	}
 
-	else {
-		pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`)
-	}
+		command = arr.join(' ');
 
-	dbhelper.connectToDb();
-	dbhelper.insertUser(message.author.id, 1);
-	dbhelper.closeDb();
-	
-	
-	//pokeEmbed.addField('Types', types);
-    message.channel.send({ embeds: [pokeEmbed] });
-	pokeEmbed = new MessageEmbed();
-	//message.channel.send(pokemon);
-  }
+		let pokemon = list.filter(poke => poke.name === command)[0];
+
+		if (pokemon == undefined) {
+			return;
+		}
+
+		pokeEmbed.setTitle(`${pokemon.name}`);
+
+		let types = ""
+		// Capitalize first letter of each type
+		for (let i = 0; i < pokemon.types.length; i++) {
+			if (i == 1) types += '/';
+			types += pokemon.types[i].charAt(0).toUpperCase() + pokemon.types[i].slice(1);
+		}
+
+		pokeEmbed.setDescription(types);
+
+		debugger;
+		let fastMovesField = ""
+
+		// Creates a field for each charge move 
+		for (let i = 0; i < pokemon.counts.length; i++) {
+			for (let j = 0; j < pokemon.counts[i].fastMoves.length; j++) {
+				debugger;
+				fastMovesField += `**${pokemon.counts[i].fastMoves[j].name}**: ${pokemon.counts[i].fastMoves[j].counts}\n`; 
+			}
+			pokeEmbed.addField(pokemon.counts[i].chargedMove.name, fastMovesField, true);
+			fastMovesField = "";
+		}
+
+		//message.channel.send(command);
+
+		if (arrThumb.length > 1) {
+
+			switch(arrThumb[1]) {
+				case 'galarian':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1].slice(0, -3)}.gif`);
+					break;
+				case 'alolan':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1].slice(0, -1)}.gif`);
+					break;
+				case 'therian':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
+					break;
+				case 'armoured':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`);
+					break;
+				case 'sunny':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
+					break;
+				case 'rainy':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
+					break;
+				case 'snowy':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
+					break;
+				case 'normal':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`);
+					break;
+				case 'male':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`);
+					break;
+				case 'female':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-f.gif`);
+					break;
+				/*ase 'crowned':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1]}.gif`);
+					break;
+				case 'hisuian':
+					pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}-${arrThumb[1].slice(0, -2)}.gif`);
+					break;*/
+				default:
+					break;
+			}
+		}
+
+		else {
+			pokeEmbed.setThumbnail(`https://play.pokemonshowdown.com/sprites/ani/${arrThumb[0]}.gif`)
+		}
+
+		dbhelper.connectToDb();
+		dbhelper.insertUser(message.author.id, 1);
+		dbhelper.closeDb();
+		
+		
+		//pokeEmbed.addField('Types', types);
+		message.channel.send({ embeds: [pokeEmbed] });
+		pokeEmbed = new MessageEmbed();
+		//message.channel.send(pokemon);
+	}
 });
 
 var pokeEmbed = new MessageEmbed()
