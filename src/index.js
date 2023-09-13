d3d5246708cc233169fe6107c42e69a2c6677019
swiftcast <4298986+swiftcast@users.noreply.github.com>
2023-09-13 04:04:13 -0400

    Moves event listeners to their own files

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 // attaching a .commands property to
 // client instance so that it can access your commands in other files
 client.commands = new Collection();
@@ -28,30 +29,18 @@ for (const folder of commandFolders) {
 		}
 	}
 }
-client.once(Events.ClientReady, () => {
-	console.log('Ready!');
-});
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
+}
 
 client.login(process.env.TOKEN);
