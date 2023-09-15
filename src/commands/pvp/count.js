be5339e799cad10ea296cb2e2ac6b7257bac5eaf
swiftcast <4298986+swiftcast@users.noreply.github.com>
2023-09-14 21:05:53 -0400

    Add type emotes to embed title

--- src/commands/pvp/count.js
+++ src/commands/pvp/count.js
@@ -60,12 +60,18 @@ let getCountsEmbed = function(pokemonInput, form) {
         .setDescription(`The Pokemon "${pokemonInput}" was not found.`);
   }
 
+  const typeString = (pokemonObj.types || []).map(type => {
+    const emote = typeEmote[type.toLowerCase()];
+    return emote ? `<:${type}:${emote}>` : type; // Use emote if available, or type name
+}).join(' ');
+
+
   const pokeEmbed = new EmbedBuilder()
 	.setColor(0x0099FF)
-	.setTitle(pokemonObj.name)
+	.setTitle(`${pokemonObj.name} ${typeString}`)
 	//.setURL('https://discord.js.org/')
 	//.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-	.setDescription('Some description here')
+	//.setDescription('Some description here')
 	.setThumbnail(generateThumbnailURL(pokemonObj.name) )
 	/*.addFields(
 		{ name: 'Regular field title', value: 'Some value here' },
@@ -77,7 +83,7 @@ let getCountsEmbed = function(pokemonInput, form) {
 	//.setImage('https://i.imgur.com/AfFp7pu.png')
 	.setTimestamp()
 	.setFields(getMovesFields(pokemonObj))
-	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+	//.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
  return pokeEmbed
 }
@@ -99,3 +105,24 @@ function generateThumbnailURL(pokemonName, thumbType) {
 		return `${baseUrl}${pokemonName.toLowerCase()}.gif`;
 	}
   }
+
+  const typeEmote = {
+	normal: '952453832938500146',
+	water: '952453832762347542',
+	grass: '952453832883961866',
+	electric: '952453832875606056',
+	ice: '952453832816869377',
+	fighting: '952453832825274378',
+	fire: '952453832837849209',
+	poison: '952453832976261180',
+	ground: '952453832422617160',
+	flying: '952453832888156210',
+	psychic: '952453832951070770',
+	bug: '952453832439377972',
+	rock: '952453833018179614',
+	ghost: '952453832485527564',
+	dark: '952453832825241640',
+	dragon: '952453833500553276',
+	steel: '952453832883994654',
+	fairy: '952453832611360810'
+}
