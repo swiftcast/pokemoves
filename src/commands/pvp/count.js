0b207cdedb70fbdfd9402869be0f0e7e2bf0d4b2
swiftcast <4298986+swiftcast@users.noreply.github.com>
2023-10-26 01:45:36 -0400

    Fix update command to update list obj

--- src/commands/pvp/count.js
+++ src/commands/pvp/count.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const fs = require('fs');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
 
 //let buildhelper = require ("../../build")
 //const list = buildhelper.buildData();
@@ -7,284 +7,304 @@ const fs = require('fs');
 let list = reloadJsonData();
 
 module.exports = {
-	reloadJsonData,
-	data: new SlashCommandBuilder()
-        .setName('count')
-        .setDescription('Replies with move counts for a Pokemon')
-        .addStringOption(option =>
-            option.setName('pokemon')
-                .setDescription('The Pokemon whose moves are to be returned')
-                .setMaxLength(200)
-                .setRequired(true))
-		.addStringOption(option =>
-			option.setName('form')
-			.setDescription('Pokemon form to return')
-			.setAutocomplete(true)),
-	async autocomplete(interaction) {
-		const formOptions = formsArray
+  list,
+  reloadJsonData,
+  data: new SlashCommandBuilder()
+    .setName("count")
+    .setDescription("Replies with move counts for a Pokemon")
+    .addStringOption((option) =>
+      option
+        .setName("pokemon")
+        .setDescription("The Pokemon whose moves are to be returned")
+        .setMaxLength(200)
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("form")
+        .setDescription("Pokemon form to return")
+        .setAutocomplete(true)
+    ),
+  async autocomplete(interaction) {
+    const formOptions = formsArray;
 
-		const focusedValue = interaction.options.getFocused();
-		const filtered = formOptions.filter(formOption => formOption.toLowerCase().startsWith(focusedValue.toLowerCase()));
+    const focusedValue = interaction.options.getFocused();
+    const filtered = formOptions.filter((formOption) =>
+      formOption.toLowerCase().startsWith(focusedValue.toLowerCase())
+    );
 
-		await interaction.respond(
-			filtered.map(formOption => ({ name: formOption, value: formOption })),
-		);
-	},
-    async execute(interaction) {
-		const pokemon = interaction.options.getString('pokemon') ?? 'wigglytuff';
+    await interaction.respond(
+      filtered.map((formOption) => ({ name: formOption, value: formOption }))
+    );
+  },
+  async execute(interaction) {
+    const pokemon = interaction.options.getString("pokemon") ?? "wigglytuff";
 
-		const form = interaction.options.getString('form')
+    const form = interaction.options.getString("form");
 
-    	const pokeEmbed = getCountsEmbed(pokemon,form)
-		await interaction.reply({ embeds: [pokeEmbed]});
-	},
+    const pokeEmbed = getCountsEmbed(pokemon, form);
+    await interaction.reply({ embeds: [pokeEmbed] });
+  },
 };
 
-let getCountsEmbed = function(pokemonInput, forme) {
-	let pokemonObj = getPokemonObj((forme ? `${pokemonInput} (${forme})` : pokemonInput))
-	let form = forme ?? ''
-	if (!pokemonObj) {
-	  return new EmbedBuilder()
-		  .setColor(0xFF0000) // Red color for error
-		  .setTitle('Pokemon not found')
-		  .setDescription(`The Pokemon "${pokemonInput}" was not found.`);
-	}
-  
-	const typeString = (pokemonObj.types || []).map(type => {
-	  const emote = typeEmote[type.toLowerCase()];
-	  return emote ? `<:${type}:${emote}>` : type; // Use emote if available, or type name
-  }).join(' ');
-  
-  
-	const pokeEmbed = new EmbedBuilder()
-	  .setColor(0x0099FF)
-	  .setTitle(`${pokemonObj.name} ${typeString}`)
-	  //.setURL('https://discord.js.org/')
-	  //.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-	  //.setDescription('Some description here')
-	  .setTimestamp()
-	  .setFields(getMovesFields(pokemonObj))
-	  //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-	  try {
-  	pokeEmbed.setThumbnail(generateThumbnailURL(pokemonInput.toLowerCase(), form.toLowerCase()) )
-	  }
-	  catch(error) {
-		console.log(error)
-	  }
-   return pokeEmbed
+let getCountsEmbed = function (pokemonInput, forme) {
+  let pokemonObj = getPokemonObj(
+    forme ? `${pokemonInput} (${forme})` : pokemonInput
+  );
+  let form = forme ?? "";
+  if (!pokemonObj) {
+    return new EmbedBuilder()
+      .setColor(0xff0000) // Red color for error
+      .setTitle("Pokemon not found")
+      .setDescription(`The Pokemon "${pokemonInput}" was not found.`);
   }
 
+  const typeString = (pokemonObj.types || [])
+    .map((type) => {
+      const emote = typeEmote[type.toLowerCase()];
+      return emote ? `<:${type}:${emote}>` : type; // Use emote if available, or type name
+    })
+    .join(" ");
+
+  const pokeEmbed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle(`${pokemonObj.name} ${typeString}`)
+    //.setURL('https://discord.js.org/')
+    //.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    //.setDescription('Some description here')
+    .setTimestamp()
+    .setFields(getMovesFields(pokemonObj));
+  //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+  try {
+    pokeEmbed.setThumbnail(
+      generateThumbnailURL(pokemonInput.toLowerCase(), form.toLowerCase())
+    );
+  } catch (error) {
+    console.log(error);
+  }
+  return pokeEmbed;
+};
+
 function getPokemonObj(command) {
-  return list.pokemon.filter(poke => poke.name.toLowerCase() === command.toLowerCase())[0];
+  return list.pokemon.filter(
+    (poke) => poke.name.toLowerCase() === command.toLowerCase()
+  )[0];
 }
 
 function getMovesFields(pokemonObj) {
-    let chargedMoves = pokemonObj.chargedMoveIds;
-    let fastMoves = pokemonObj.fastMoveIds;
-    let fields = [];
+  let chargedMoves = pokemonObj.chargedMoveIds;
+  let fastMoves = pokemonObj.fastMoveIds;
+  let fields = [];
 
-    // Loop through charged moves
-    for (let i = 0; i < chargedMoves.length; i++) {
-        let chargedMoveName = abbreviateString(list.moves[chargedMoves[i]].name);
-        let fastMovesField = "";
+  // Loop through charged moves
+  for (let i = 0; i < chargedMoves.length; i++) {
+    let chargedMoveName = abbreviateString(list.moves[chargedMoves[i]].name);
+    let fastMovesField = "";
 
-		if (pokemonObj.name === "Mew") {
-        // Loop through fast moves for each charged move
-			for (let j = 0; j < fastMoves.length; j++) {
-				fastMovesField += `**${abbreviateString(list.moves[fastMoves[j]].name)}**: ${list.counts[fastMoves[j]][chargedMoves[i]]}\n`;
-			}
-		}
-		else {
-			for (let j = 0; j < fastMoves.length; j++) {
-				const emote = typeEmote[list.moves[fastMoves[j]].type];
-				const type = list.moves[fastMoves[j]].type
-				fastMovesField += `**${abbreviateString(list.moves[fastMoves[j]].name)}**: ${list.counts[fastMoves[j]][chargedMoves[i]]}\n`;
-			}
-		}
-
-		const emote = typeEmote[list.moves[chargedMoves[i]].type];
-		const type = list.moves[chargedMoves[i]].type
-
-        // Add the charged move and its corresponding fast moves to the fields
-        fields.push({
-            name: `<:${type}:${emote}> ${chargedMoveName}`,
-            value: fastMovesField,
-            inline: true, 
-        });
+    if (pokemonObj.name === "Mew") {
+      // Loop through fast moves for each charged move
+      for (let j = 0; j < fastMoves.length; j++) {
+        fastMovesField += `**${abbreviateString(
+          list.moves[fastMoves[j]].name
+        )}**: ${list.counts[fastMoves[j]][chargedMoves[i]]}\n`;
+      }
+    } else {
+      for (let j = 0; j < fastMoves.length; j++) {
+        const emote = typeEmote[list.moves[fastMoves[j]].type];
+        const type = list.moves[fastMoves[j]].type;
+        fastMovesField += `**${abbreviateString(
+          list.moves[fastMoves[j]].name
+        )}**: ${list.counts[fastMoves[j]][chargedMoves[i]]}\n`;
+      }
     }
-	
-    return fields;
+
+    const emote = typeEmote[list.moves[chargedMoves[i]].type];
+    const type = list.moves[chargedMoves[i]].type;
+
+    // Add the charged move and its corresponding fast moves to the fields
+    fields.push({
+      name: `<:${type}:${emote}> ${chargedMoveName}`,
+      value: fastMovesField,
+      inline: true,
+    });
+  }
+
+  return fields;
 }
-
-
-
 
 function generateThumbnailURL(pokemonName, form) {
-	const baseUrl = 'https://play.pokemonshowdown.com/sprites/ani/';
-	
-	// Replace any periods or dashes in the Pokémon name
-	const sanitizedPokemonName = pokemonName.replace(/[\s.'’-]/g, '');
+  const baseUrl = "https://play.pokemonshowdown.com/sprites/ani/";
 
-  
-	switch (form) {
-	  case 'galarian':
-		return `${baseUrl}${sanitizedPokemonName}-${form.slice(0, -3)}.gif`;
-	  case 'alolan':
-		return `${baseUrl}${sanitizedPokemonName}-${form.slice(0, -1)}.gif`;
-	  case 'therian':
-	  case 'sunny':
-	  case 'rainy':
-	  case 'snowy':
-		return `${baseUrl}${sanitizedPokemonName}-${form}.gif`;
-	  case 'normal':
-	  case 'male':
-		return `${baseUrl}${sanitizedPokemonName}.gif`;
-	  case 'female':
-		return `${baseUrl}${sanitizedPokemonName}-f.gif`;
-	  default:
-		return `${baseUrl}${sanitizedPokemonName}.gif`;
-	}
+  // Replace any periods or dashes in the Pokémon name
+  const sanitizedPokemonName = pokemonName.replace(/[\s.'’-]/g, "");
+
+  switch (form) {
+    case "galarian":
+      return `${baseUrl}${sanitizedPokemonName}-${form.slice(0, -3)}.gif`;
+    case "alolan":
+      return `${baseUrl}${sanitizedPokemonName}-${form.slice(0, -1)}.gif`;
+    case "therian":
+    case "sunny":
+    case "rainy":
+    case "snowy":
+    case "mega":
+    case "heat":
+    case "wash":
+    case "frost":
+    case "fan":
+    case "mow":
+      return `${baseUrl}${sanitizedPokemonName}-${form}.gif`;
+    case "normal":
+    case "male":
+      return `${baseUrl}${sanitizedPokemonName}.gif`;
+    case "female":
+      return `${baseUrl}${sanitizedPokemonName}-f.gif`;
+    default:
+      return `${baseUrl}${sanitizedPokemonName}.gif`;
   }
-
-  function reloadJsonData() {
-	// Reload your JSON data here
-	try {
-	  const data = fs.readFileSync('../data/pokemovesdata.json', 'utf8');
-	  console.log("JSON reloaded.")
-	  return JSON.parse(data);
-	} catch (error) {
-	  console.error('Error reloading JSON data:', error.message);
-	  throw new Error('An error occurred while reloading the JSON data.');
-	}
-  }
-
-  function abbreviateString(input) {
-	const words = input.split(' ');
-  
-	if (words.length === 1) {
-	  return input;  // Return the input as is for a single word
-	} else {
-	  // Abbreviate all words except the last one
-	  const abbreviatedWords = words.slice(0, -1).map(word => word.charAt(0).toUpperCase() + '.');
-	  const lastWord = words[words.length - 1];  // Keep the last word as is
-  
-	  return [...abbreviatedWords, lastWord].join(' ');
-	}
-  }
-
-  const typeEmote = {
-	normal: '952453832938500146',
-	water: '952453832762347542',
-	grass: '952453832883961866',
-	electric: '952453832875606056',
-	ice: '952453832816869377',
-	fighting: '952453832825274378',
-	fire: '952453832837849209',
-	poison: '952453832976261180',
-	ground: '952453832422617160',
-	flying: '952453832888156210',
-	psychic: '952453832951070770',
-	bug: '952453832439377972',
-	rock: '952453833018179614',
-	ghost: '952453832485527564',
-	dark: '952453832825241640',
-	dragon: '952453833500553276',
-	steel: '952453832883994654',
-	fairy: '952453832611360810'
 }
 
+function reloadJsonData() {
+  // Reload your JSON data here
+  try {
+    const data = fs.readFileSync("../data/pokemovesdata.json", "utf8");
+    console.log("JSON reloaded.");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reloading JSON data:", error.message);
+    throw new Error("An error occurred while reloading the JSON data.");
+  }
+}
+
+function abbreviateString(input) {
+  const words = input.split(" ");
+
+  if (words.length === 1) {
+    return input; // Return the input as is for a single word
+  } else {
+    // Abbreviate all words except the last one
+    const abbreviatedWords = words
+      .slice(0, -1)
+      .map((word) => word.charAt(0).toUpperCase() + ".");
+    const lastWord = words[words.length - 1]; // Keep the last word as is
+
+    return [...abbreviatedWords, lastWord].join(" ");
+  }
+}
+
+const typeEmote = {
+  normal: "952453832938500146",
+  water: "952453832762347542",
+  grass: "952453832883961866",
+  electric: "952453832875606056",
+  ice: "952453832816869377",
+  fighting: "952453832825274378",
+  fire: "952453832837849209",
+  poison: "952453832976261180",
+  ground: "952453832422617160",
+  flying: "952453832888156210",
+  psychic: "952453832951070770",
+  bug: "952453832439377972",
+  rock: "952453833018179614",
+  ghost: "952453832485527564",
+  dark: "952453832825241640",
+  dragon: "952453833500553276",
+  steel: "952453832883994654",
+  fairy: "952453832611360810",
+};
+
 const formsArray = [
-	'10%',
-	'50%',
-	'Alolan',
-	'Altered',
-	'Aria',
-	'Armoured',
-	'Attack',
-	'Average Size',
-	'Baile',
-	'Black',
-	'Bug',
-	'Burn',
-	'Chill',
-	'Complete',
-	'Confined',
-	'Core',
-	'Crowned Shield',
-	'Crowned Sword',
-	'Dark',
-	'Dawn Wings',
-	'Defense',
-	'Douse',
-	'Dragon',
-	'Dusk',
-	'Dusk Mane',
-	'Electric',
-	'Eternamax',
-	'Fairy',
-	'Fan',
-	'Female',
-	'Fighting',
-	'Fire',
-	'Flying',
-	'Frost',
-	'Galarian',
-	'Ghost',
-	'Grass',
-	'Ground',
-	'Heat',
-	'Hero',
-	'Hero of Many Battles',
-	'Hisuian',
-	'Ice',
-	'Ice Rider',
-	'Incarnate',
-	'Land',
-	'Large Size',
-	'Libre',
-	'Male',
-	'Mega',
-	'Mega X',
-	'Mega Y',
-	'Midday',
-	'Midnight',
-	'Mow',
-	'Noice',
-	'Normal',
-	'Origin',
-	'Overcast',
-	'Pa’u',
-	'Pirouette',
-	'Plant',
-	'Poison',
-	'Pom-Pom',
-	'Pop Star',
-	'Primal',
-	'Psychic',
-	'Rainy',
-	'Rapid Strike',
-	'Rock',
-	'Rock Star',
-	'Sandy',
-	'School',
-	'Sensu',
-	'Shadow Rider',
-	'Shock',
-	'Single Strike',
-	'Sky',
-	'Small Size',
-	'Snowy',
-	'Solo',
-	'Speed',
-	'Steel',
-	'Sunny',
-	'Super Size',
-	'Therian',
-	'Trash',
-	'Ultra',
-	'Unbound',
-	'Wash',
-	'Water',
-	'White',
-	'Zero'
+  "10%",
+  "50%",
+  "Alolan",
+  "Altered",
+  "Aria",
+  "Armoured",
+  "Attack",
+  "Average Size",
+  "Baile",
+  "Black",
+  "Bug",
+  "Burn",
+  "Chill",
+  "Complete",
+  "Confined",
+  "Core",
+  "Crowned Shield",
+  "Crowned Sword",
+  "Dark",
+  "Dawn Wings",
+  "Defense",
+  "Douse",
+  "Dragon",
+  "Dusk",
+  "Dusk Mane",
+  "Electric",
+  "Eternamax",
+  "Fairy",
+  "Fan",
+  "Female",
+  "Fighting",
+  "Fire",
+  "Flying",
+  "Frost",
+  "Galarian",
+  "Ghost",
+  "Grass",
+  "Ground",
+  "Heat",
+  "Hero",
+  "Hero of Many Battles",
+  "Hisuian",
+  "Ice",
+  "Ice Rider",
+  "Incarnate",
+  "Land",
+  "Large Size",
+  "Libre",
+  "Male",
+  "Mega",
+  "Mega X",
+  "Mega Y",
+  "Midday",
+  "Midnight",
+  "Mow",
+  "Noice",
+  "Normal",
+  "Origin",
+  "Overcast",
+  "Pa’u",
+  "Pirouette",
+  "Plant",
+  "Poison",
+  "Pom-Pom",
+  "Pop Star",
+  "Primal",
+  "Psychic",
+  "Rainy",
+  "Rapid Strike",
+  "Rock",
+  "Rock Star",
+  "Sandy",
+  "School",
+  "Sensu",
+  "Shadow Rider",
+  "Shock",
+  "Single Strike",
+  "Sky",
+  "Small Size",
+  "Snowy",
+  "Solo",
+  "Speed",
+  "Steel",
+  "Sunny",
+  "Super Size",
+  "Therian",
+  "Trash",
+  "Ultra",
+  "Unbound",
+  "Wash",
+  "Water",
+  "White",
+  "Zero",
 ];
--- src/commands/pvp/update.js
+++ src/commands/pvp/update.js
@@ -1,69 +1,75 @@
-const { SlashCommandBuilder } = require('discord.js');
-const axios = require('axios');
-const fs = require('fs/promises'); // Using promises-based fs module for async/await
-const { reloadJsonData } = require('./count');
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+const fs = require("fs/promises"); // Using promises-based fs module for async/await
+const { list, reloadJsonData } = require("./count"); //list of pokemon,moves,counts
 
-const jsonUrl = 'https://www.pokemoves.com/data.json'
-const localJsonFilePath = '../data/pokemovesdata.json';
+const jsonUrl = "https://www.pokemoves.com/data.json";
+const localJsonFilePath = "../data/pokemovesdata.json";
 // Function to check if the JSON from the URL is newer than the one on disk
 async function isJsonNewerOnUrl() {
+  try {
+    const response = await axios.get(jsonUrl);
+    const urlTimestamp = response.data.timestamp;
+
+    try {
+      const localData = await fs.readFile(localJsonFilePath, "utf8");
+      const localJson = JSON.parse(localData);
+      const localTimestamp = localJson.timestamp;
+
+      return urlTimestamp > localTimestamp;
+    } catch (error) {
+      // Handle the case where the local JSON file doesn't exist
+      return true;
+    }
+  } catch (error) {
+    console.error("Error fetching JSON data from URL:", error.message);
+    return false;
+  }
+}
+
+async function updateJsonFromUrl() {
+  const isNewer = await isJsonNewerOnUrl();
+
+  if (isNewer) {
     try {
       const response = await axios.get(jsonUrl);
-      const urlTimestamp = response.data.timestamp;
-  
-      try {
-        const localData = await fs.readFile(localJsonFilePath, 'utf8');
-        const localJson = JSON.parse(localData);
-        const localTimestamp = localJson.timestamp;
-  
-        return urlTimestamp > localTimestamp;
-      } catch (error) {
-        // Handle the case where the local JSON file doesn't exist
-        return true;
-      }
-    } catch (error) {
-      console.error('Error fetching JSON data from URL:', error.message);
-      return false;
-    }
-  }
-  
-  async function updateJsonFromUrl() {
-    const isNewer = await isJsonNewerOnUrl();
-  
-    if (isNewer) {
-      try {
-        const response = await axios.get(jsonUrl);
-        const jsonData = response.data;
-  
-        // Write the JSON data to the local file using fs/promises.writeFile
-        await fs.writeFile(localJsonFilePath, JSON.stringify(jsonData, null, 2), 'utf8');
-  
-        console.log('JSON data updated successfully.');
+      const jsonData = response.data;
 
-        reloadJsonData()
-        return 'JSON data updated successfully.'
-      } catch (error) {
-        console.error('Error:', error.message);
-      }
-    } else {
-      console.log('Local JSON file is up to date.');
-      return 'Local pokemoves JSON file is up to date.'
+      // Write the JSON data to the local file using fs/promises.writeFile
+      await fs.writeFile(
+        localJsonFilePath,
+        JSON.stringify(jsonData, null, 2),
+        "utf8"
+      );
+
+      console.log("JSON data updated successfully.");
+
+      list = reloadJsonData();
+      return "JSON data updated successfully.";
+    } catch (error) {
+      console.error("Error:", error.message);
     }
+  } else {
+    console.log("Local JSON file is up to date.");
+    return "Local pokemoves JSON file is up to date.";
   }
-  
-  module.exports = {
-    data: new SlashCommandBuilder()
-      .setName('update')
-      .setDescription('Updates pokemoves data JSON.'),
-    async execute(interaction) {
-      await interaction.deferReply(); // Defer the reply while updating the JSON
-  
-      try {
-        let msg = await updateJsonFromUrl();
-        await interaction.editReply(msg);
-      } catch (error) {
-        console.error('Error updating JSON:', error.message);
-        await interaction.editReply('An error occurred while updating the JSON file.');
-      }
-    },
-  };
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("update")
+    .setDescription("Updates pokemoves data JSON."),
+  async execute(interaction) {
+    await interaction.deferReply(); // Defer the reply while updating the JSON
+
+    try {
+      let msg = await updateJsonFromUrl();
+      await interaction.editReply(msg);
+    } catch (error) {
+      console.error("Error updating JSON:", error.message);
+      await interaction.editReply(
+        "An error occurred while updating the JSON file."
+      );
+    }
+  },
+};
